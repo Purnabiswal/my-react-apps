1410faf368dc947973f484a507c6d42c322c60a7
Purnabiswal <purnabiswal824@gmail.com>
2026-06-19 15:06:04 +0530

    New Changes

---
+++ 04-cards-project/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './component/Card'
+
+const App = () => {
+  return (
+    <div>
+      
+      <div className='card'>
+        <Card/>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
