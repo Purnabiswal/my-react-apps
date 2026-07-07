6590a3233ea573b2db0a14624c546238f181c388
Purnabiswal <purnabiswal824@gmail.com>
2026-07-07 11:21:08 +0530

    Notes Project Completed

--- 13-notes-app/src/App.jsx
+++ 13-notes-app/src/App.jsx
@@ -1,50 +1,47 @@
 import React, { useEffect, useState } from "react";
+import { X } from "lucide-react";
 
 const App = () => {
-
-
-  const [title, settitle] = useState('');
-  const [deatails, setdeatails] = useState('');
+  const [title, settitle] = useState("");
+  const [details, setdetails] = useState("");
 
   const [task, settask] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
     // console.log(title);
-    createTask(title, deatails);
 
-    settitle('')
-    setdeatails('')
+    const copyTask = [...task];
+    copyTask.push({ title, details });
 
-    
+    settask(copyTask);
+
+    settitle("");
+    setdetails("");
   };
 
-  const createTask=(title, deatails)=>{
-  
-    const newTask={
-      title,
-      deatails
-    }
-    settask(preevTask=>[...preevTask, newTask])
-    
+
+  const deleteNote=(idx)=>{
+    const copyTask=[...task]
+    // console.log(copyTask[idx]);
+    copyTask.splice(idx,1)
+    settask(copyTask)
+
   }
 
-  useEffect(()=>{
-    console.log(task);
-    
-  },[task])
+  // useEffect(() => {
+  //   // console.log(task);
+  // }, [task]);
 
   return (
     <div className="h-screen lg:flex bg-black text-white">
       <form
         onSubmit={(e) => {
           submitHandler(e);
-
         }}
         className="flex  lg:w-1/2 items-start flex-col gap-4 p-10"
       >
         <h1 className="text-4xl font-bold">Add Notes</h1>
-
 
         {/* Heading */}
 
@@ -53,25 +50,23 @@ const App = () => {
           className="w-full px-5 py-2 font-medium border-2 rounded-2xl outline-none"
           placeholder="Enter Notes Heading"
           value={title}
-          onChange={(e)=>{
-            settitle(e.target.value)
+          onChange={(e) => {
+            settitle(e.target.value);
           }}
         />
-
-
 
         {/* Details */}
         <textarea
           type="text"
           className="h-32 w-full font-medium flex flex-row items-start px-5 py-2 border-2 rounded-2xl outline-none"
           placeholder="Enter Details"
-          value={deatails}
-          onChange={(e)=>{
-            setdeatails(e.target.value)
+          value={details}
+          onChange={(e) => {
+            setdetails(e.target.value);
           }}
         />
 
-        <button className="w-full font-medium bg-white text-black px-5 py-2 rounded outline-none">
+        <button className="w-full font-medium bg-white active:scale-95 text-black px-5 py-2 rounded outline-none">
           Add Note
         </button>
         {/* <img
@@ -83,13 +78,31 @@ const App = () => {
 
       <div className=" lg:w-1/2  gap-5  flex-wrap p-10 lg:border-l-2 ">
         <h1 className="text-4xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap gap-5 mt-5 h-full overflow-auto">
-          <div className="h-52 w-40 rounded-2xl bg-white p-2 "> 
-            <h1 className="text-black text-3xl">{task[0].title}</h1>
-            <h3 className="text-gray-700 text-xl mt-2">{task[0].details}</h3>
-          </div>
-          <div className="h-52 w-40 rounded-2xl bg-white "> </div>
-          <div className="h-52 w-40 rounded-2xl bg-white "> </div>
+        <div className="flex flex-wrap gap-5 items-start justify-start mt-5 h-[90%] overflow-auto">
+          {task.map((elem, idx) => {
+            return (
+              <div
+                key={idx}
+                className="h-52 w-40 text-black  rounded-xl bg-cover relative
+                bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]  px-4 py-9"
+              >
+                <h1 
+                onClick={()=>{
+                  deleteNote(idx)
+                }}
+                className="bg-red-500 w-min absolute top-5 right-5 rounded-full p-1 active:scale-95">
+                  <X />
+                </h1>
+
+                <h1 className="text-lg leading-tight font-bold">
+                  {elem.title}
+                </h1>
+                <p className="leading-tight text-sm font-medium text-gray-500 mt-2">
+                  {elem.details}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
