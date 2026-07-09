ed40478f127c29b6bfc3e82c16161c70dbbb893e
Purnabiswal <purnabiswal824@gmail.com>
2026-07-09 10:23:14 +0530

    Learnt localstorage

---
+++ 14-localstorage/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const App = () => {
+  
+  // localStorage.setItem("theme", "dark")
+  // localStorage.setItem("user", "Purna")
+
+  // const user=localStorage.getItem('user')
+  // console.log(user);
+  
+  // localStorage.removeItem('theme')
+
+
+
+
+
+
+  // //With object
+  // const user={
+  //   username:'Purna',
+  //   age:'21'
+  // }
+
+  // localStorage.setItem("user", JSON.stringify(user))
+
+  // const item=JSON.parse(localStorage.getItem('user')) //JSON.parse() converts string to object
+  
+  // console.log(typeof item);
+  
+  
+  
+  return (
+    <div>
+      App
+    </div>
+  )
+}
+
+export default App
