bf8d63332904da514c642c61798600c7843bd9ca
Purnabiswal <purnabiswal824@gmail.com>
2026-07-11 12:06:38 +0530

    Change theme from child to parent added

---
+++ 20-bonus-topic/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = (props) => {
+  return (
+    <div>
+        <h1>{props.theme}</h1>
+      <button onClick={()=>{
+        props.setTheme('dark')
+      }}>Change theme</button>
+    </div>
+  )
+}
+
+export default Navbar
